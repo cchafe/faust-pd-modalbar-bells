d87dde830721366b6c494b92aedfda7749e6cddf
cchafe <chafe@stanford.edu>
2013-05-17 12:46:37 -0700

    add sh/

---
+++ sh/buildOnMac.sh
@@ -0,0 +1,10 @@
+cd dsp
+cp ../../faustDir/instrument.* .
+cp ../../faustDir/*.h .
+cp -r ../../faustDir/faust .
+
+../../faustDir/sys/bin/faust -a puredata.cpp -o $1.cpp $1.dsp
+
+c++ -DPD -O2 -funroll-loops -fomit-frame-pointer -fPIC -Wall -msse -I/Applications/Pd-extended.app/Contents/Resources/include/  -I/opt/local/include -bundle -m32 -undefined suppress -flat_namespace -I ./ -Dmydsp=$1 $1.cpp -o $1~.pd_darwin
+
+cp $1~.pd_darwin ../pd/
